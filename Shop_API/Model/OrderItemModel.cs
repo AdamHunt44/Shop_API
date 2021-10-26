ce40c804e58cb3d58a26dec639fda3bc8433037f
Adam Hunt <adamhunt1998@gmail.com>
2021-10-26 15:38:42 +0100

    Removed unused field ProductSize

--- Shop_API/Model/OrderItemModel.cs
+++ Shop_API/Model/OrderItemModel.cs
@@ -18,7 +18,6 @@ namespace Shop_API.Model
         public int ProductId { get; set; }
 
         public string ProductCategory { get; set; }
-        public string ProductSize { get; set; }
         public string ProductName { get; set; }
     }
 }
