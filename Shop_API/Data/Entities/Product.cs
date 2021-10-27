215e35c640db980b4ea3ef75a423f6c640db1bcd
Adam Hunt <adamhunt1998@gmail.com>
2021-10-27 15:54:18 +0100

    Updated product price to use decimal instead of double format

--- Shop_API/Data/Entities/Product.cs
+++ Shop_API/Data/Entities/Product.cs
@@ -10,7 +10,7 @@ namespace Shop_API.Data.Entities
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string Description { get; set; }
         public int Quantity { get; set; }
         public string Category { get; set; }
