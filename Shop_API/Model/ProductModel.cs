84e0c31181df68b7f5cbf399767292a379ab0186
Adam Hunt <adam.hunt@ijs-abstract.com>
2021-10-27 12:29:49 +0100

    Removed nullable string implementation

--- Shop_API/Model/ProductModel.cs
+++ Shop_API/Model/ProductModel.cs
@@ -15,7 +15,7 @@ namespace Shop_API.Model
         [StringLength(50)]
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public int Quantity { get; set; }
         public string Category { get; set; }
     }
