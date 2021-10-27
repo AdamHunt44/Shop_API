49a87e94b00a7a73469ba3b619a69b9bdcd456f8
Adam Hunt <adamhunt1998@gmail.com>
2021-10-27 14:20:15 +0100

    Added Reflection dependancy in Startup.cs for automapper

--- Shop_API/Startup.cs
+++ Shop_API/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Shop_API.Data;
-using Shop_API.Data.Repositories;
 using System.Reflection;
 
 namespace Shop_API
