f83e0ce0618daa2796e24314d8b6cc9bfa121f73
Adam Hunt <adamhunt1998@gmail.com>
2021-10-28 12:04:42 +0100

    Implemented method to get all Order Items

--- Shop_API/Controllers/OrderItemsController.cs
+++ Shop_API/Controllers/OrderItemsController.cs
@@ -2,10 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Shop_API.Data;
+using Shop_API.Model;
+using System;
+using System.Threading.Tasks;
 
 namespace Shop_API.Controllers
 {
     [Route("/api/orders/{orderid}/items")]
+    [Route("/api/items")]
     [ApiController]
     public class OrderItemsController : ControllerBase
     {
@@ -18,6 +22,24 @@ namespace Shop_API.Controllers
             _repository = repository;
             _logger = logger;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<OrderItemModel[]>> GetAllOrderItems()
+        {
+            try
+            {
+                var results = await _repository.GetAllOrderItemsAsync();
+
+                OrderItemModel[] allOrderItems = _mapper.Map<OrderItemModel[]>(results);
+
+                return Ok(allOrderItems);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
         }
     }
 }
